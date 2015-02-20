69c0d1d7c5db554e08bfb1e7d556a8d80bd1edff
Thomas Riboulet <riboulet@gmail.com>
2015-02-20 08:05:08 +0000

    adding values does help

--- test_bases_test.go
+++ test_bases_test.go
@@ -4,6 +4,10 @@ import "testing"
 
 func BenchmarkSearch1000(b *testing.B) {
 	s := make([]int, 1000)
+	for i := 0; i < 1000; i++ {
+		s[i] = i + 1
+	}
+
 	for n := 0; n < b.N; n++ {
 		Search(s, 3)
 	}
